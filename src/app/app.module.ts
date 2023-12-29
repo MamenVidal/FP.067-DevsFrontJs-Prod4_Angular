4e65816c5f412509b2d018479f378a4f4a99f78b
Mamen Vidal <mvidalda@uoc.edu>
2023-12-29 16:05:47 +0100

    config firebase emulator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 // Importamos elementos y configuración de Firebase
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FirestoreModule , getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getMessaging, provideMessaging, getToken, onMessage } from "@angular/fire/messaging";
 import { environment } from '../environments/environment';
+import { Functions, FunctionsModule, getFunctions, provideFunctions } from '@angular/fire/functions';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import { connectFirestoreEmulator } from 'firebase/firestore';
+import { connectStorageEmulator } from 'firebase/storage';
+import { connectFunctionsEmulator } from 'firebase/functions';
+
 
 const customNotifierOptions: NotifierOptions = {
   position: {
@@ -68,8 +73,30 @@ const customNotifierOptions: NotifierOptions = {
     AppRoutingModule,
     // Creamos la aplicación de Firebase con nuestra configuración
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
+    //provideFirestore(() => getFirestore()),
+    //provideStorage(() => getStorage()),
+    //provideMessaging(() => getMessaging()),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(firestore, environment.emulatorConfig.firestore.host, environment.emulatorConfig.firestore.port);
+      }
+      return firestore;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.useEmulators) {
+        connectStorageEmulator(storage, environment.emulatorConfig.storage.host, environment.emulatorConfig.storage.port);
+      }
+      return storage;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions();
+      if (environment.useEmulators) {
+        connectFunctionsEmulator(functions, environment.emulatorConfig.functions.host, environment.emulatorConfig.functions.port);
+      }
+      return functions;
+    }),
     provideMessaging(() => getMessaging()),
     // Agregamos notificaciones
     NotifierModule.withConfig(customNotifierOptions),
